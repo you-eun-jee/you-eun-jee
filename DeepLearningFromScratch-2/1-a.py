5afa24eba6386fe0026136318b788bdb48d941b2
EJ You <98823664+you-eun-jee@users.noreply.github.com>
2022-05-22 22:30:19 +0900

    changed by PIP-8

--- DeepLearningFromScratch-2/1-a.py
+++ DeepLearningFromScratch-2/1-a.py
@@ -22,7 +22,7 @@ print(W.shape)
 print(W.ndim)
 # 2
 
-X = np.array([[0,1,2],[3,4,5]])
+X = np.array([[0, 1 ,2], [3, 4, 5]])
 
 # 1.1.2 행렬의 원소별 연산
 print(W + X)
@@ -30,7 +30,7 @@ print(X * W)
 
 # 1.1.3 브로드캐스트
 
-A = np.array([[1,2],[3,4]])
+A = np.array([[1, 2], [3, 4]])
 print(A * 10)
 
 
